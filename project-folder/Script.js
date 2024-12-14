4f3af16b4af1c2764c565ab567198425916e529d
Ahmed Shebl <126363070+theflash8778@users.noreply.github.com>
2024-12-14 20:07:35 +0200

    Add files via upload

---
+++ project-folder/Script.js
@@ -0,0 +1,9 @@
+$(document).ready(function () {
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 50) {
+      $("header").addClass("sticky");
+    } else {
+      $("header").removeClass("sticky");
+    }
+  });
+});
